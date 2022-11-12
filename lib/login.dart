e08f4673001907ef50d4dbb144557e87cb60923d
Logan Cardoso <102470246+Logan-cic@users.noreply.github.com>
2022-11-12 15:11:48 -0300

    Login screen and supporting images
    The login screen was created but still without this link to Firebase and an images folder was also created to store the project images

---
+++ lib/login.dart
@@ -0,0 +1,85 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(color: Color(0xff075E54)),
+        padding: const EdgeInsets.all(16),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 32),
+                  child:
+                      Image.asset("images/logo.png", width: 200, height: 150),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 8),
+                  child: TextField(
+                    autofocus: true,
+                    keyboardType: TextInputType.emailAddress,
+                    style: TextStyle(fontSize: 20),
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                        hintText: "E-mail",
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32))),
+                  ),
+                ),
+                TextField(
+                  keyboardType: TextInputType.text,
+                  style: TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      hintText: "Password",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(32))),
+                ),
+                Padding(
+                    padding: EdgeInsets.only(top: 16, bottom: 10),
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                          foregroundColor: Colors.green,
+                          padding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(32))),
+                      child: Text(
+                        "Login",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      onPressed: (() {}),
+                    )),
+                Center(
+                  child: GestureDetector(
+                    child: Text(
+                      "Don't have an account? register here.",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onTap: (() {}),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,30 @@
-import 'package:firebase_core/firebase_core.dart';
+//import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'Home.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:whatsapp/login.dart';
+//import 'Home.dart';
 
-void main() async {
+void main() {
+  runApp(const MaterialApp(
+    home: Login(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+
+//Test done to initialize Firebase
+
+/*void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   FirebaseFirestore.instance
-  .collection("usuarios")
+  .collection("users")
   .doc("001")
-  .set({"nome": "Logan", "idade": "22"});
+  .set({"name": "Logan", "yaer": "22"});
 
   runApp(const MaterialApp(
     home: Home(),
     debugShowCheckedModeBanner: false,
   ));
 }
+*/
