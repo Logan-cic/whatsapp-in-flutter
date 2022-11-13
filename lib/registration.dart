044f5b5e7322c051332f9b76abab728881104f71
Logan Cardoso <102470246+Logan-cic@users.noreply.github.com>
2022-11-13 14:44:30 -0300

    user registration validation functions

--- lib/registration.dart
+++ lib/registration.dart
@@ -8,13 +8,49 @@ class Registration extends StatefulWidget {
 }
 
 class _RegistrationState extends State<Registration> {
+  //Controllers
+  TextEditingController _controllerName = TextEditingController();
+  TextEditingController _controllerEmail = TextEditingController();
+  TextEditingController _controllerPassword = TextEditingController();
+  String _errorMessage = " ";
+
+  _authentication() {
+    //retrieve the fields
+    String name = _controllerName.text;
+    String email = _controllerEmail.text;
+    String password = _controllerPassword.text;
+
+    if (name.isNotEmpty && name.length > 2) {
+      if (email.isNotEmpty && email.contains("@")) {
+        if (password.isNotEmpty && password.length > 6) {
+          setState(() {
+            _errorMessage = " ";
+          });
+          _userRegistration();
+        } else {
+          setState(() {
+            _errorMessage = "Enter a valid password";
+          });
+        }
+      } else {
+        setState(() {
+          _errorMessage = "Enter a valid email";
+        });
+      }
+    } else {
+      setState(() {
+        _errorMessage = "Enter a valid name";
+      });
+    }
+  }
+
+  _userRegistration() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Registration"),
-        backgroundColor: Color(0xff075E54)
-      ),
+          title: Text("Registration"), backgroundColor: Color(0xff075E54)),
       body: Container(
         decoration: BoxDecoration(color: Color(0xff075E54)),
         padding: const EdgeInsets.all(16),
@@ -31,6 +67,7 @@ class _RegistrationState extends State<Registration> {
                 Padding(
                   padding: EdgeInsets.only(bottom: 8),
                   child: TextField(
+                    controller: _controllerName,
                     autofocus: true,
                     keyboardType: TextInputType.text,
                     style: TextStyle(fontSize: 20),
@@ -46,6 +83,7 @@ class _RegistrationState extends State<Registration> {
                 Padding(
                   padding: EdgeInsets.only(bottom: 8),
                   child: TextField(
+                    controller: _controllerEmail,
                     keyboardType: TextInputType.emailAddress,
                     style: TextStyle(fontSize: 20),
                     decoration: InputDecoration(
@@ -58,6 +96,8 @@ class _RegistrationState extends State<Registration> {
                   ),
                 ),
                 TextField(
+                  controller: _controllerPassword,
+                  obscureText: true,
                   keyboardType: TextInputType.text,
                   style: TextStyle(fontSize: 20),
                   decoration: InputDecoration(
@@ -80,8 +120,16 @@ class _RegistrationState extends State<Registration> {
                         "Registration",
                         style: TextStyle(color: Colors.white, fontSize: 20),
                       ),
-                      onPressed: (() {}),
+                      onPressed: (() {
+                        _authentication();
+                      }),
                     )),
+                Center(
+                  child: Text(
+                    _errorMessage,
+                    style: const TextStyle(color: Colors.red, fontSize: 20),
+                  ),
+                )
               ],
             ),
           ),
